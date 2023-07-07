4f8b1f7a8ba35c000d7c5edea190ce473bf7c04d
Michel Medina Tena <124810680+micheee24@users.noreply.github.com>
2023-07-07 13:51:52 -0700

    Update to Style

--- components/home.js
+++ components/home.js
@@ -1,11 +1,16 @@
-function Home(){
+function Home() {
   return (
     <Card
-      txtcolor="black"
+      bgcolor="info"
+      txtcolor="white"
       header="BadBank Landing Module"
       title="Welcome to the bank"
       text="You can move around using the navigation bar."
-      body={(<img src="images/bank.png" className="img-fluid" alt="Responsive image"/>)}
-    />    
-  );  
+      body={
+        <img
+          src="images/bank.png"
+          className="img-fluid"
+          alt="Responsive image"/>}
+    />
+  );
 }
